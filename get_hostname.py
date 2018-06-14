5b9d0d3614dce7579ec9ea308ad73fb4d2e568b9
Vladislav <40007228+Kowareta@users.noreply.github.com>
2018-06-14 20:56:00 +0400

    Update get_hostname.py
    Added some logging

--- get_hostname.py
+++ get_hostname.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from datetime import datetime, date, time
 import socket
+import logging
+import sys
 
 class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
     #GET
     def do_GET(self):
+
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+
         #Send response status code
         self.send_response(200)
 
@@ -12,19 +18,38 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
 
-        #Send message back to client
-        message = (socket.gethostname())
+        #Send hostname back to client
+        hostname = (socket.gethostname())
         #Write contens as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
+        self.wfile.write(bytes(hostname, "utf8"))
         return
 
-    def run():
-      print('Starting server...')
+    #Write stduot/stderr into logfile
+    def log_request(self, code='-', size='-'):
 
-      #Server settings
-      server_address = ('', 80)
-      httpd = HTTPServer(server_address, HTTPServer_RequestHandler)
-      print('Running server...')
-      httpd.serve_forever()
+        logging.info(("%s - - [%s] %s" %
+              (self.address_string(),
+               self.log_date_time_string(),
+               '"%s" %s %s' % (self.requestline, str(code), str(size))
+               )
+        ))
 
-    run()
+def run():
+
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y.%m.%d %H:%M:%S:', filename='gethostname.log', level=logging.DEBUG)
+    #print('Starting server...')
+    logging.info('Starting server...')
+    #Server settings
+    server_address = ('', 80)
+    httpd = HTTPServer(server_address, HTTPServer_RequestHandler)
+    #print('Server is running...')
+    logging.info('Server is running...')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    #print('Stopping server...')
+    logging.info('Stopping server...')
+
+run()
